6b02ef56c9393ed853e313e9f99481bc45383eb0
SLCW <hr@skylightcodeworks.com>
2026-02-05 15:59:03 +0500

    Update settings.py

--- ai-research-tools/config/settings.py
+++ ai-research-tools/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", "skyapollo.skylightcodeworks.com"]
 
 
 # Application definition
